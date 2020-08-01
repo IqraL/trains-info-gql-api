c2469cf0d75716891e6d3484abc576f3dce94385
IqraL <IqraLatif159@gmail.com>
2020-08-01 19:43:48 +0100

    gql server setuo

--- schema/index.js
+++ schema/index.js
@@ -4,10 +4,25 @@ const GraphQLJSON = require("graphql-type-json");
 module.exports = gql`
   scalar JSON
 
+  type cctv {
+    location: String
+    points: [Float]
+  }
+  type Library {
+    name: String
+    address: String
+    postcode: String
+    telepone: String
+    points: [Float]
+  }
+  type railwayStation {
+    name: String
+    points: [Float]
+  }
   type Query {
     getConservationAreas: JSON
-    getCctvLocations: JSON
-    getLibraries: JSON
-    getRailwayStations: JSON
+    getCctvLocations: [cctv]
+    getLibraries: [Library]
+    getRailwayStations: [railwayStation]
   }
 `;
