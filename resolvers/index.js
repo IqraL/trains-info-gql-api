e63d68346418ce050a9d472ddd7df5848917d71a
IqraL <IqraLatif159@gmail.com>
2020-08-01 20:00:30 +0100

    inital getdeparturBoard request

--- resolvers/index.js
+++ resolvers/index.js
@@ -1,8 +1,45 @@
+const axios = require("axios");
 module.exports = {
   Query: {
-    getRailwayStations: async () => {},
-    getLibraries: () => {},
-    getCctvLocations: () => {},
-    getConservationAreas: async () => {},
+    getDepartureBoard: async () => {
+      let xmls = `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:typ="http://thalesgroup.com/RTTI/2013-11-28/Token/types" xmlns:ldb="http://thalesgroup.com/RTTI/2017-10-01/ldb/">
+   <soapenv:Header>
+      <typ:AccessToken>
+         <typ:TokenValue>7e8e81a0-405a-4235-ad83-e96ce3a78eee</typ:TokenValue>
+      </typ:AccessToken>
+   </soapenv:Header>
+   <soapenv:Body>
+      <ldb:GetDepBoardWithDetailsRequest>
+         <ldb:numRows>5</ldb:numRows>
+         <ldb:crs>RCD</ldb:crs>
+         <!--Optional:-->
+         <ldb:filterCrs>MSO</ldb:filterCrs>
+         <!--Optional:-->
+         <ldb:filterType>to</ldb:filterType>
+         <!--Optional:-->
+         <ldb:timeOffset>0</ldb:timeOffset>
+         <!--Optional:-->
+         <ldb:timeWindow>120</ldb:timeWindow>
+      </ldb:GetDepBoardWithDetailsRequest>
+   </soapenv:Body>
+</soapenv:Envelope>`;
+
+      await axios
+        .post(
+          "https://lite.realtime.nationalrail.co.uk/OpenLDBWS/ldb11.asmx",
+          xmls,
+          {
+            headers: { "Content-Type": "text/xml" },
+          }
+        )
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+
+      return null;
+    },
   },
 };
--- schema/index.js
+++ schema/index.js
@@ -4,25 +4,7 @@ const GraphQLJSON = require("graphql-type-json");
 module.exports = gql`
   scalar JSON
 
-  type cctv {
-    location: String
-    points: [Float]
-  }
-  type Library {
-    name: String
-    address: String
-    postcode: String
-    telepone: String
-    points: [Float]
-  }
-  type railwayStation {
-    name: String
-    points: [Float]
-  }
   type Query {
-    getConservationAreas: JSON
-    getCctvLocations: [cctv]
-    getLibraries: [Library]
-    getRailwayStations: [railwayStation]
+    getDepartureBoard: JSON
   }
 `;
